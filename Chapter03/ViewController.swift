617256a78743f452b2fed1d630e09f65740293c7
yoon so young <yunso86@naver.com>
2018-07-01 18:28:12 +0900

    imageView

--- Chapter03/ViewController.swift
+++ Chapter03/ViewController.swift
@@ -25,7 +25,25 @@ class ViewController: UIViewController {
     
     */
     
+    @IBOutlet weak var imageView: UIImageView!
     
     
+    
+    @IBAction func btnZoom(_ sender: UIButton) {
+        if sender.currentTitle == "확대" {
+            sender.setTitle("축소", for: .normal)
+            imageView.frame.size.width *= 2
+            imageView.frame.size.height *= 2
+        }else{
+            sender.setTitle("확대", for: .normal)
+            imageView.frame.size.width /= 2
+            imageView.frame.size.height /= 2
+        }
+        
+    }
+    
+    @IBAction func switchOnOff(_ sender: UISwitch) {
+        imageView.image = sender.isOn ? #imageLiteral(resourceName: "lamp_on.png") : #imageLiteral(resourceName: "lamp_off")
+    }
 }
 
